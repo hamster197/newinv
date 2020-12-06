56ce638b21b9a0b6faeef96a919f9115a27d0f73
hamster197 <hamster197@mail.ru>
2020-12-06 16:11:53 +0300

    maps

--- crm/admin.py
+++ crm/admin.py
@@ -22,7 +22,7 @@ class flatfields(admin.ModelAdmin):
     search_fields = ['pk']
     fields = ['status_obj','type','raion','kr_raion','cena_agenstv','adress','dom_numb','kvart_numb','etag',#,
               'etagnost','author','client_name','client_tel','prim','domclick', 'youtube',
-              'contract', 'contract_number', 'contract_date_end', ]
+              'contract', 'contract_number', 'contract_date_end', 'uc_dom_nunb' ]
     #ordering = ('adress','dom_numb',)
 
 class flatgalfields(admin.ModelAdmin):
